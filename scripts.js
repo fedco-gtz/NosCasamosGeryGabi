21b8c09d7e60645a5959ab84004118cd11006672
Federico Gabriel Gutierrez <125905957+fedco-gtz@users.noreply.github.com>
2023-09-27 19:15:13 -0300

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,55 @@
+
+
+
+// Confirmar Asistencia parallax
+$('.parallax-confirmar').parallax({
+  imageSrc: 'img/confirmar-asistencia.jpg'
+
+});
+
+// Portada parallax
+$('.parallax-portada').parallax({
+  imageSrc: 'assets/img/'
+});
+
+// ----------------------
+
+
+
+// Cuenta Regresiva:
+
+// Set the date we're counting down to
+// var countDownDate = new Date(fechaCuentaRegresiva).getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function () {
+
+//   // Get today's date and time
+//   var now = new Date().getTime();
+
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Display the result in the element with id="demo"
+//   document.getElementById("reloj").innerHTML = days + " días " + hours + "hs " +
+//     minutes + "m " + seconds + "s ";
+
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("reloj").innerHTML = "¡LLEGO EL GRAN DÍA!";
+//     $('#reloj').prev('p').html('Listo...');
+//   }
+// }, 1000);
+
+
+
+// ----------------------
+
+
